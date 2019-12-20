0ba85fb8d5da19a09e0bab1dbb07266474c964a2
Liko <li_xie1992@hotmail.com>
2019-12-20 17:59:22 -0500

    trimmed intervalslider a bit

--- src/components/IntervalSlider.js
+++ src/components/IntervalSlider.js
@@ -4,33 +4,29 @@ import { Slider } from "react-semantic-ui-range";
 
 const IntervalSlider = ({ intervalSpeed, setIntervalSpeed }) => {
   return (
-    <div>
-      <Grid stackable stretched={true}>
-        <Grid.Column width={12}>
-          <Segment>
-            <p>
-              <Slider
-                color="orange"
-                inverted={false}
-                settings={{
-                  start: intervalSpeed,
-                  min: 100,
-                  max: 2000,
-                  step: 100,
-                  onChange: value => setIntervalSpeed(value)
-                }}
-              />
-            </p>
-          </Segment>
-        </Grid.Column>
-        <GridColumn width={4}>
-          <Label color="orange">
-            <p>Interval:</p>
-            <p>{intervalSpeed / 1000}s </p>
-          </Label>
-        </GridColumn>
-      </Grid>
-    </div>
+    <Grid stackable stretched={true}>
+      <Grid.Column width={12}>
+        <Segment>
+          <Slider
+            color="orange"
+            inverted={false}
+            settings={{
+              start: intervalSpeed,
+              min: 100,
+              max: 2000,
+              step: 100,
+              onChange: value => setIntervalSpeed(value)
+            }}
+          />
+        </Segment>
+      </Grid.Column>
+      <GridColumn width={4}>
+        <Label color="orange">
+          <p>Interval:</p>
+          <p>{intervalSpeed / 1000}s </p>
+        </Label>
+      </GridColumn>
+    </Grid>
   );
 };
 
