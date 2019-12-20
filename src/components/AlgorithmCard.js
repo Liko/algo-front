18a99e26fe95a90312bd5f0a8f99d0ed628df40a
Liko <li_xie1992@hotmail.com>
2019-12-20 10:09:09 -0500

    added a provisional incrementing step timer as props to AlgorithmCard

--- src/components/AlgorithmCard.js
+++ src/components/AlgorithmCard.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 import AlgorithmDisplayBar from "./AlgorithmDisplayBar";
 
 const AlgorithmCard = ({ algo, removeAlgo, step }) => {
+  const [currentData, setCurrentData] = useState(algo.steps[step]);
+
   return (
     <Card fluid onClick={() => removeAlgo(algo.key)}>
       <Card.Content>
+        <p>{step}</p>
         <p>{algo.name}</p>
         <AlgorithmDisplayBar />
         <AlgorithmDisplayBar />
--- src/containers/AlgorithmsContainer.js
+++ src/containers/AlgorithmsContainer.js
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, Grid } from "semantic-ui-react";
 import ControllerContainer from "./ControllerContainer";
 import AlgorithmCard from "../components/AlgorithmCard";
 
 const AlgorithmsContainer = ({ algos, removeAlgo }) => {
   const [step, setStep] = useState(0);
+  let intervalSpeed = 1000;
+
+  const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+      const stepTick = () => {
+        savedCallback.current();
+      };
+      if (delay !== null) {
+        let id = setInterval(stepTick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  };
+
+  useInterval(() => {
+    setStep(step + 1);
+  }, intervalSpeed);
 
   const buildAlgorithmCards = () => {
     // Make a card for each algorithm a user has selected
