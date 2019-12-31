5fc0b84a034a6ce76bcbb384306e7deaf6927259
Liko <li_xie1992@hotmail.com>
2019-12-31 12:19:27 +0000

    cleanup

--- src/algos/selectionSort.js
+++ src/algos/selectionSort.js
@@ -6,7 +6,6 @@ import cloneDeep from "lodash/cloneDeep";
 // currently used in selection sort
 const addStep = (i, jOrMin, array, steps, status) => {
   let step = cloneDeep(array);
-  // add status for each step[index] is incorrect. must refactor lines 10-11.
   step[i].status = status;
   step[jOrMin].status = status;
   steps.push(step);
@@ -21,7 +20,6 @@ const selectionSort = (size = 10) => {
   // Finally, perform a selection sort on the shuffled array
   for (let i = 0; i < arr.length - 1; i++) {
     let min = i;
-    // addStep(i, arr, steps, "checking");
 
     for (let j = i + 1; j < arr.length; j++) {
       addStep(min, j, arr, steps, "checking");
@@ -36,7 +34,7 @@ const selectionSort = (size = 10) => {
       addStep(i, min, arr, steps, "moving");
     }
   }
-  // }
+
   steps.push(arr);
   return steps;
 };
