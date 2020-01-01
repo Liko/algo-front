076dee1bbf19285bb6fca6ccd8867e3afdc01ac9
geewey <geewey@gmail.com>
2020-01-01 18:03:16 +0000

    implemented isSorted logic to insertionSort

--- src/algos/insertionSort.js
+++ src/algos/insertionSort.js
@@ -1,17 +1,21 @@
 import {
   generateRandomArray,
-  addCheckOrSwapStep
+  addCheckOrSwapStep,
+  addInsertStep
 } from "../util/algorithmHelper";
+
+import cloneDeep from "lodash/cloneDeep";
 
 const insertionSort = (size = 10) => {
   const arr = generateRandomArray(size);
 
   // Initialize steps, which will keep track of array element positions at each cycle
-  const steps = [[...arr]];
+  const steps = [cloneDeep(arr)];
 
   // Finally, perform a insertion sort on the shuffled array
   // Begin at index position 1
   let i = 1;
+  arr[0].isSorted = true;
   while (i < arr.length) {
     /// Initialize x as the key element for comparison
     let x = arr[i];
@@ -21,11 +25,12 @@ const insertionSort = (size = 10) => {
     while (j >= 0 && arr[j].value > x.value) {
       // Swap occurs here
       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      addCheckOrSwapStep(j, j + 1, arr, steps, "moving");
+      addInsertStep(j, arr, steps, "moving");
       j = j - 1;
     }
     // Key element is inserted here
     arr[j + 1] = x;
+    arr[j + 1].isSorted = true;
     // Increments index here
     i = i + 1;
   }
